1000cde77707e21079ed2aa226a4b88844beef58
ziliwang912 <ziliwang912@gmailc.om>
2019-12-27 19:06:22 -0500

    One bug found

--- MultibaseCalculator.java
+++ MultibaseCalculator.java
@@ -13,6 +13,10 @@ import javax.swing.JFrame;
  * 
  * <h2>Continued Project</h2>
  * There are potential bugs in this app. This is the continued develepment for this project.
+ * Bugs:
+ * 	1. Not display numbers correctly when moving knobs back and forth
+ * 	2. 
+ * 
  * @author Zili Wang
  * @since 12-27-2019
  */
